1fe9de02fb4c79a56d85b475e6a8d8b472e639cc
Tanuj <50631065+ashx23@users.noreply.github.com>
2022-05-26 04:54:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+number = int(input("Which number do you want to check? "))
+
+
+
+check = number % 2
+if check == 0:
+  print("This number is an even number.")
+else:
+  print("This number is an odd number.")
+
+
